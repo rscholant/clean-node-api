3fd9498cefde4e2b079746b37c34c0f0cbe67641
Rafael Scholant <rafael.scholant@fortbrasil.com.br>
2021-11-16 09:25:19 -0300

    Revert "chore: add new eslint config"
    This reverts commit f72d1fb89b44fa4e82e21a619cc5e3a26cb9112a.

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,5 +1,4 @@
 import {SignUpController} from './signup';
-
 describe('SignUp Controller', () => {
   test('should return 400 if no name is provided', () => {
     const sut = new SignUpController();
