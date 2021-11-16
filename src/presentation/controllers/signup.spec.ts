a3c2d0e9290aa3df339e04463521fa363b171028
Rafael Scholant <rafael.scholant@fortbrasil.com.br>
2021-11-16 09:32:14 -0300

    chore: add new eslint config

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,15 +1,15 @@
-import {SignUpController} from './signup';
+import { SignUpController } from './signup'
 describe('SignUp Controller', () => {
   test('should return 400 if no name is provided', () => {
-    const sut = new SignUpController();
+    const sut = new SignUpController()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
         password: 'any_password',
-        passwordConfirmation: 'any_password',
-      },
-    };
-    const httpResponse = sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(400);
-  });
-});
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
